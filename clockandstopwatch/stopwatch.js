8fb75640285fd86fe24dbe6ff96bc53ab91dfee0
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-13 03:17:22 +0500

    stopwatch done

--- clockandstopwatch/stopwatch.js
+++ clockandstopwatch/stopwatch.js
@@ -1,22 +1,50 @@
-const time = document.getElementById("time");
-const timeValue = time.innerHTML;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+const startbtn = document.getElementById("startbtn");
 const stopbtn = document.getElementById("stopbtn");
 const resetbtn = document.getElementById("resetbtn");
+let intervalId;
 
-setInterval(function () {
-    console.log(timeValue.split(":"))
-    var [hours, minutes, seconds, milliseconds] = timeValue.split(":");
+const interval = () => {
     milliseconds++;
-    if (Math.floor(milliseconds / 1000) > 1) {
+    if (milliseconds >= 1000) {
+        milliseconds = 0;
         seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes >= 60) {
+                minutes = 0;
+                hours++;
+                document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
+            }
+            document.getElementById("minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
+        }
+        document.getElementById("seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
     }
+    document.getElementById("milliseconds").innerHTML = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+};
 
-    if (Math.floor(seconds / 60) > 1) {
-        minutes++;
-    }
+startbtn.addEventListener("click", () => {
+    intervalId = setInterval(interval, 1);
+    startbtn.disabled = true;
+    stopbtn.disabled = false;
+});
 
-    if (Math.floor(minutes / 60) > 1) {
-        hours++;
-    }
-    time.innerHTML = `${hours}: ${minutes}: ${seconds}: ${milliseconds}`
-}, 1000)
+stopbtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    startbtn.disabled = false;
+    stopbtn.disabled = true;
+});
+
+resetbtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    document.getElementById("milliseconds").innerHTML = "000";
+    document.getElementById("seconds").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    startbtn.disabled = false;
+    stopbtn.disabled = true;
+});
