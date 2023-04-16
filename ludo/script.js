85d84cfadd7e82468aa90066a7327c0989d10321
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-17 04:00:48 +0500

    initial commit

--- ludo/script.js
+++ ludo/script.js
@@ -6,6 +6,13 @@ const img1 = document.getElementById("img-1");
 const img2 = document.getElementById("img-2");
 const p = document.querySelector("p");
 
+playBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input1.value === "" || input2.value === "") {
+        alert("naam to dalo");
+    }
+})
+
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     if (input1.value === "" || input2.value === "") {
@@ -13,16 +20,16 @@ submitBtn.addEventListener("click", (e) => {
     }
     else {
         playBtn.addEventListener("click", (e) => {
+            e.preventDefault();
             var score1 = parseInt(Math.random() * 6 + 1);
             var score2 = parseInt(Math.random() * 6 + 1);
-            e.preventDefault();
             img1.setAttribute("src", `./${score1}.jpeg`);
             img2.setAttribute("src", `./${score2}.jpeg`);
             if (score1 > score2) {
-                p.innerText = "Player 1 wins"
+                p.innerText = `${input1.value} wins`;
             }
             else if (score1 < score2) {
-                p.innerText = "Player 2 wins"
+                p.innerText = `${input2.value} wins`
             }
             else {
                 p.innerText = "Tie"
