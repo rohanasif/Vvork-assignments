3b19ab342296860a7c66800339e2a54797432134
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-26 11:48:44 +0500

    initial commit

---
+++ drumkit/index.js
@@ -0,0 +1,22 @@
+window.addEventListener("keydown", function (e) {
+    console.log(e.keyCode);
+    var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    var key = document.querySelector(`.key[data-key="${e.keyCode}]"`)
+    if (audio) {
+        audio.play();
+        audio.currentTime = 0;
+        key.classList.add("playing");
+    }
+    return;
+});
+
+var kk = document.querySelectorAll(".key");
+kk.forEach((e) => {
+    e.addEventListener("transitionend", remove)
+})
+function remove(k) {
+    if (k.propertyName = "transform") {
+        this.classList.remove("playing");
+    }
+    return;
+}
