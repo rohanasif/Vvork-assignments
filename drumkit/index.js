200565fd6e36c6e34e27f368ef2e0114c24e7a73
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-26 13:18:12 +0500

    drumkit complete

--- drumkit/index.js
+++ drumkit/index.js
@@ -1,18 +1,16 @@
-window.addEventListener("keydown", function (e) {
-    console.log(e.keyCode);
-    var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    var key = document.querySelector(`.key[data-key="${e.keyCode}]"`)
+window.addEventListener("keydown", (e) => {
+    var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    var key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (audio) {
         audio.play();
         audio.currentTime = 0;
-        key.classList.add("playing");
+        key.classList.add("playing")
     }
     return;
-});
-
+})
 var kk = document.querySelectorAll(".key");
 kk.forEach((e) => {
-    e.addEventListener("transitionend", remove)
+    e.addEventListener('transitionend', remove);
 })
 function remove(k) {
     if (k.propertyName = "transform") {
