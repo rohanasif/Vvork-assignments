0bfcb547a6687a9f8e48ce4556a73b711c08776b
rohanasif1990 <rohanasif1990@gmail.com>
2023-05-03 10:49:59 +0500

    some changes

--- localstorage_list/script.js
+++ localstorage_list/script.js
@@ -4,7 +4,7 @@ var div = document.querySelector("div");
 btn.addEventListener("click", () => {
     var a = JSON.parse(localStorage.getItem("data"));
     if (a) {
-        console.log(a);
+        div.innerHTML = inp.value;
         a.push(inp.value);
         localStorage.setItem("data", JSON.stringify(a));
     }
@@ -13,14 +13,14 @@ btn.addEventListener("click", () => {
     }
 })
 
-var showData = JSON.parse(localStorage.getItem("data"));
-if (showData) {
-    showData.map((el, i) => {
-        var li = document.createElement("li");
-        li.innerHTML = `${el}<button onclick="handleClick(${i})">edit</button>`;
-        div.appendChild(li);
-    })
-}
-function handleClick(index) {
-    console.log(index)
-}
+// var showData = JSON.parse(localStorage.getItem("data"));
+// if (showData) {
+//     showData.map((el, i) => {
+//         var li = document.createElement("li");
+//         li.innerHTML = `${el}<button onclick="handleClick(${i})">edit</button>`;
+//         div.appendChild(li);
+//     })
+// }
+// function handleClick(index) {
+//     console.log(index)
+// }
