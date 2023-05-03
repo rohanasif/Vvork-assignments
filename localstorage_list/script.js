b5431b81d3ffcf49880f770da4a12a226f01d50f
rohanasif1990 <rohanasif1990@gmail.com>
2023-05-03 13:29:34 +0500

    done

--- localstorage_list/script.js
+++ localstorage_list/script.js
@@ -1,26 +1,49 @@
-var inp = document.querySelector("input");
-var btn = document.querySelector("button");
-var div = document.querySelector("div");
-btn.addEventListener("click", () => {
-    var a = JSON.parse(localStorage.getItem("data"));
+var inp = document.querySelector('input')
+var btn = document.querySelector('button')
+var div = document.querySelector('div')
+var updateBtn = document.getElementById('btn')
+btn.addEventListener('click', () => {
+    var a = JSON.parse(localStorage.getItem('data'))
     if (a) {
-        div.innerHTML = inp.value;
-        a.push(inp.value);
-        localStorage.setItem("data", JSON.stringify(a));
+        a.push(inp.value)
+        localStorage.setItem('data', JSON.stringify(a))
     }
     else {
-        localStorage.setItem("data", JSON.stringify([inp.value]));
+        localStorage.setItem('data', JSON.stringify([inp.value]))
+
     }
+    inp.value = ""
+    showData()
 })
 
-// var showData = JSON.parse(localStorage.getItem("data"));
-// if (showData) {
-//     showData.map((el, i) => {
-//         var li = document.createElement("li");
-//         li.innerHTML = `${el}<button onclick="handleClick(${i})">edit</button>`;
-//         div.appendChild(li);
-//     })
-// }
-// function handleClick(index) {
-//     console.log(index)
-// }
+function showData() {
+    var myData = JSON.parse(localStorage.getItem('data'))
+    if (myData) {
+        div.innerHTML = ""
+        myData.forEach((ele, index) => {
+            var li = document.createElement('li')
+            li.innerHTML = `${ele} <button class="button" onclick=handleEdit(${index})>edit</button> <button class="button" onclick=handleDel(${index})>del</button>`
+            div.appendChild(li)
+        });
+    }
+}
+function handleDel(i) {
+    var a = JSON.parse(localStorage.getItem('data'))
+    a.splice(i, 1)
+    localStorage.setItem('data', JSON.stringify(a))
+    showData()
+}
+var handleIndex = -1;
+function handleEdit(i) {
+    var a = JSON.parse(localStorage.getItem('data'))
+    inp.value = a[i]
+    handleIndex = i;
+}
+
+updateBtn.addEventListener('click', () => {
+    var a = JSON.parse(localStorage.getItem('data'))
+    a[handleIndex] = inp.value;
+    localStorage.setItem('data', JSON.stringify(a))
+    showData()
+    inp.value = ""
+})
