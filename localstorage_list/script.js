fbec468e81031c2b8e9671f35bb52890a871789d
rohanasif1990 <rohanasif1990@gmail.com>
2023-05-03 10:14:51 +0500

    create separate script

---
+++ localstorage_list/script.js
@@ -0,0 +1,26 @@
+var inp = document.querySelector("input");
+var btn = document.querySelector("button");
+var div = document.querySelector("div");
+btn.addEventListener("click", () => {
+    var a = JSON.parse(localStorage.getItem("data"));
+    if (a) {
+        console.log(a);
+        a.push(inp.value);
+        localStorage.setItem("data", JSON.stringify(a));
+    }
+    else {
+        localStorage.setItem("data", JSON.stringify([inp.value]));
+    }
+})
+
+var showData = JSON.parse(localStorage.getItem("data"));
+if (showData) {
+    showData.map((el, i) => {
+        var li = document.createElement("li");
+        li.innerHTML = `${el}<button onclick="handleClick(${i})">edit</button>`;
+        div.appendChild(li);
+    })
+}
+function handleClick(index) {
+    console.log(index)
+}
