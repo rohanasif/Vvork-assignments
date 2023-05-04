debbf54301ab15de0d154b0a58a845e447463725
rohanasif1990 <rohanasif1990@gmail.com>
2023-05-04 12:13:34 +0500

    some progress

--- registration-form/script.js
+++ registration-form/script.js
@@ -1,9 +1,10 @@
 const data = []
-const obj = {}
 const inputs = document.querySelectorAll("input");
 const submitBtn = document.querySelector("button");
 const tbody = document.querySelector("tbody");
+
 submitBtn.addEventListener("click", () => {
+    const obj = {};
     inputs.forEach(input => {
         if (input.type === "text") {
             obj.name = input.value;
@@ -17,8 +18,21 @@ submitBtn.addEventListener("click", () => {
     })
     data.push(obj);
     localStorage.setItem("data", JSON.stringify(data));
+    showData();
+});
+
+function showData() {
     const allData = JSON.parse(localStorage.getItem("data"));
+    let html = "";
     for (let person of allData) {
-        console.log(person.name, "all data through person");
+        html +=
+            `<tr>
+                <td>${person.name}</td>
+                <td>${person.email}</td>
+                <td><button>Edit</button><td>
+            </tr>`;
     }
-})
+    tbody.insertAdjacentHTML("beforeend", html);
+}
+
+showData();
