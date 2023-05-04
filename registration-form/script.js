2f4bbb73125bca64428a29d89bb800fc59b5e379
rohanasif1990 <rohanasif1990@gmail.com>
2023-05-04 20:03:00 +0500

    working with refresh

--- registration-form/script.js
+++ registration-form/script.js
@@ -1,38 +1,33 @@
-const data = []
+const data = [];
 const inputs = document.querySelectorAll("input");
 const submitBtn = document.querySelector("button");
 const tbody = document.querySelector("tbody");
 
 submitBtn.addEventListener("click", () => {
-    const obj = {};
-    inputs.forEach(input => {
-        if (input.type === "text") {
-            obj.name = input.value;
-        }
-        else if (input.type === "email") {
-            obj.email = input.value;
-        }
-        else if (input.type === "password") {
-            obj.password = input.value;
-        }
-    })
+  const obj = {};
+  inputs.forEach((input) => {
+    if (input.type === "text") {
+      obj.name = input.value;
+    } else if (input.type === "email") {
+      obj.email = input.value;
+    } else if (input.type === "password") {
+      obj.password = input.value;
+    }
+  });
+  if (obj.name !== "" && obj.email !== "" && obj.password !== "") {
     data.push(obj);
     localStorage.setItem("data", JSON.stringify(data));
-    showData();
+  }
 });
-
-function showData() {
-    const allData = JSON.parse(localStorage.getItem("data"));
-    let html = "";
-    for (let person of allData) {
-        html +=
-            `<tr>
-                <td>${person.name}</td>
-                <td>${person.email}</td>
-                <td><button>Edit</button><td>
-            </tr>`;
+const allData = JSON.parse(localStorage.getItem("data"));
+if (allData) {
+  for (let person of allData) {
+    if (person.name !== "" || person.email !== "" || person.password !== "") {
+      tbody.innerHTML += `<tr>
+                                    <td>${person.name}</td>
+                                    <td>${person.email}</td>
+                                    <td><button>Edit</button><td>
+                                </tr>`;
     }
-    tbody.insertAdjacentHTML("beforeend", html);
+  }
 }
-
-showData();
