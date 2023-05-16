ca446af6b815b2d466711a1e6367d75d5e89fbec
rohanasif1990 <rohanasif1990@gmail.com>
2023-05-16 11:07:58 +0500

    done

--- login_page/forgotpassword.js
+++ login_page/forgotpassword.js
@@ -3,7 +3,6 @@ const resetBtn = document.getElementById("resetBtn");
 resetBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const newEmail = document.getElementById("new-email").value;
-  const old = document.getElementById("old").value;
   const newPassword = document.getElementById("new-password").value;
   const repeatNew = document.getElementById("repeat-new").value;
 
@@ -11,21 +10,34 @@ resetBtn.addEventListener("click", (e) => {
 
   if (allData) {
     for (user of allData) {
-      if (user.email === newEmail && user.password === old) {
-        if (newPassword === repeatNew) {
-          user.password = newPassword;
-          localStorage.setItem("data", JSON.stringify(allData));
-          alert("Password updated");
-          break;
+      if (user.email === newEmail) {
+        if (
+          newPassword.length >= 8 &&
+          /[A-Z]/.test(newPassword) &&
+          /[0-9]/.test(newPassword) &&
+          /[!@#$%^&*]/.test(newPassword)
+        ) {
+          if (newPassword === repeatNew) {
+            user.password = newPassword;
+            localStorage.setItem("data", JSON.stringify(allData));
+            alert("Password updated");
+          } else {
+            alert("Passwords don't match");
+          }
         } else {
-          alert("Password wekho!");
-          break;
+          let errorMessage = "New password must meet the following criteria:\n";
+          errorMessage += "- At least 8 characters long\n";
+          errorMessage += "- At least one uppercase letter\n";
+          errorMessage += "- At least one number\n";
+          errorMessage += "- At least one special character (!@#$%^&*)";
+          alert(errorMessage);
         }
+        break;
       } else {
-        alert("Email not found!");
+        alert("Email not found or incorrect password!");
       }
     }
   } else {
-    alert("Signup first!");
+    alert("Sign up first!");
   }
 });
--- login_page/signup.js
+++ login_page/signup.js
@@ -1,5 +1,6 @@
 const signUpBtn = document.getElementById("signUpBtn");
 const allData = [];
+
 signUpBtn.addEventListener("click", (e) => {
   const name = document.getElementById("signUpName").value;
   const email = document.getElementById("signUpEmail").value;
@@ -22,14 +23,28 @@ signUpBtn.addEventListener("click", (e) => {
     alert("Email already exists");
   } else {
     if (password === repeatPassword) {
-      const userData = {
-        name: name,
-        email: email,
-        password: password,
-      };
-      allData.push(userData);
-      localStorage.setItem("data", JSON.stringify(allData));
-      alert("You're signed up");
+      if (
+        password.length >= 8 &&
+        /[A-Z]/.test(password) &&
+        /[0-9]/.test(password) &&
+        /[!@#$%^&*]/.test(password)
+      ) {
+        const userData = {
+          name: name,
+          email: email,
+          password: password,
+        };
+        allData.push(userData);
+        localStorage.setItem("data", JSON.stringify(allData));
+        alert("You're signed up");
+      } else {
+        let errorMessage = "Password must meet the following criteria:\n";
+        errorMessage += "- At least 8 characters long\n";
+        errorMessage += "- At least one uppercase letter\n";
+        errorMessage += "- At least one number\n";
+        errorMessage += "- At least one special character (!@#$%^&*)";
+        alert(errorMessage);
+      }
     } else {
       alert("Passwords don't match");
     }
