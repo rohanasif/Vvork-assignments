a5ad2c1bef36ed0e1807eecefd46b5e2291f45c3
Rohan Asif <rohanasif1990@gmail.com>
2023-05-30 10:36:12 +0500

    cart done

--- shopping cart/script.js
+++ shopping cart/script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector("#container");
+const cartIcon = document.getElementById("cart-icon");
+const cart = document.getElementById("cart");
 
 fetch("https://fakestoreapi.com/products")
   .then((response) => response.json())
-  .then((data) => cacheData(data))
+  .then((data) => showData(data))
   .catch((error) => console.log(error));
 var mydata;
-function cacheData(d) {
+function showData(d) {
   mydata = d;
   d.forEach((item, index) => {
     const card = document.createElement("div");
@@ -30,7 +32,36 @@ function handleAdd(index) {
   if (x) {
     x.push(obj);
     localStorage.setItem("data", JSON.stringify(x));
+    var div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML = `
+                  <img class="card-img" src="${obj.image}" alt="Card image">
+                  <h5 class="card-title">${obj.title}</h5>
+                  <h5 class="card-price">$${obj.price}</h5>
+                  <button onclick= "handleDel(${obj.index})" class="delBtns">Delete</button>
+    `;
+    cart.appendChild(div);
   } else {
     localStorage.setItem("data", JSON.stringify([obj]));
+    var div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML = `
+                  <img class="card-img" src="${obj.image}" alt="Card image">
+                  <h5 class="card-title">${obj.title}</h5>
+                  <h5 class="card-price">$${obj.price}</h5>
+                  <button onclick= "handleDel(${index})" class="delBtns">Delete</button>
+    `;
+    cart.appendChild(div);
   }
+}
+
+cartIcon.addEventListener("click", () => {
+  cart.classList.toggle("hide");
+  cart.classList.toggle("show");
+});
+
+function handleDel(index) {
+  var x = JSON.parse(localStorage.getItem("data"));
+  x.splice(index, 1);
+  localStorage.setItem("data", JSON.stringify(x));
 }
