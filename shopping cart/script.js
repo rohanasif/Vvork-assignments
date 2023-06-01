28a58f58e30f191d7aab767d1f9cfff1180b5fd5
Rohan Asif <rohanasif1990@gmail.com>
2023-06-01 10:59:12 +0500

    everything works

--- shopping cart/script.js
+++ shopping cart/script.js
@@ -18,8 +18,21 @@ function showData(d) {
       <img class="card-img" src="${item.image}" alt="Card image">
       <h5 class="card-title">${item.title}</h5>
       <h5 class="card-price">$${item.price}</h5>
-      <button onclick= "handleAdd(${index})" class="addBtns">Add to cart</button>
+      <button onclick="handleAdd(${index})" class="addBtns">Add to cart</button>
     `;
+
+    // Get the count of the item from localStorage
+    var storedData = JSON.parse(localStorage.getItem("data"));
+    var itemCount = storedData ? getItemCount(storedData, item.id) : 0;
+
+    // Create a counter element
+    var counter = document.createElement("div");
+    counter.className = "item-counter";
+    counter.textContent = itemCount;
+
+    // Append the counter to the card
+    card.appendChild(counter);
+
     container.appendChild(card);
   });
 
@@ -34,7 +47,7 @@ function showData(d) {
         <img class="card-img" src="${item.image}" alt="Card image">
         <h5 class="card-title">${item.title}</h5>
         <h5 class="card-price">$${item.price}</h5>
-        <button onclick= "handleDel(${item.id})" class="delBtns">Delete</button>
+        <button onclick="handleDel(${item.id})" class="delBtns">Delete</button>
       `;
       cart.appendChild(div);
     });
@@ -50,22 +63,23 @@ function handleAdd(index) {
   };
   var x = JSON.parse(localStorage.getItem("data"));
   if (x) {
-    x.push(obj);
+    // Check if the item already exists in localStorage
+    var existingItem = x.find((item) => item.id === obj.id);
+    if (existingItem) {
+      // If the item already exists, increase its count by 1
+      existingItem.count++;
+    } else {
+      // If the item doesn't exist, add it to localStorage with a count of 1
+      obj.count = 1;
+      x.push(obj);
+    }
     localStorage.setItem("data", JSON.stringify(x));
   } else {
+    // If localStorage is empty, add the item with a count of 1
+    obj.count = 1;
     localStorage.setItem("data", JSON.stringify([obj]));
   }
-  var div = document.createElement("div");
-  div.className = "item";
-  div.id = "item-" + obj.id;
-  div.innerHTML = `
-    <img class="card-img" src="${obj.image}" alt="Card image">
-    <h5 class="card-title">${obj.title}</h5>
-    <h5 class="card-price">$${obj.price}</h5>
-    <button onclick= "handleDel(${obj.id})" class="delBtns">Delete</button>
-  `;
-  cart.appendChild(div);
-  handle();
+  updateCart();
 }
 
 cartIcon.addEventListener("click", () => {
@@ -77,11 +91,26 @@ function handleDel(id) {
   var x = JSON.parse(localStorage.getItem("data"));
   var index = x.findIndex((item) => item.id === id);
   if (index > -1) {
-    x.splice(index, 1);
+    // Decrement the count of the item by one
+    x[index].count--;
+
+    // If the count reaches zero, remove the item from the cart
+    if (x[index].count === 0) {
+      x.splice(index, 1);
+      var itemElement = document.getElementById("item-" + id);
+      itemElement.remove();
+    }
+
     localStorage.setItem("data", JSON.stringify(x));
-    var itemElement = document.getElementById("item-" + id);
-    itemElement.remove();
     handle();
+
+    // Update the counter element for the specific item
+    var counterElement = document.querySelector(`#item-${id} .item-counter`);
+
+    // Check if the item still exists in the cart
+    if (counterElement) {
+      counterElement.textContent = x[index] ? x[index].count : 0;
+    }
   }
 }
 
@@ -93,4 +122,65 @@ function handle() {
   } else {
     counter.innerHTML = "";
   }
+}
+
+function getItemCount(items, itemId) {
+  var count = 0;
+  items.forEach((item) => {
+    if (item.id === itemId) {
+      count += item.count || 1;
+    }
+  });
+  return count;
+}
+
+function updateCart() {
+  // Clear the existing cart items
+  cart.innerHTML = "";
+
+  // Retrieve items from localStorage
+  var storedData = JSON.parse(localStorage.getItem("data"));
+
+  if (storedData) {
+    storedData.forEach((item) => {
+      var div = document.createElement("div");
+      div.className = "item";
+      div.id = "item-" + item.id;
+      div.innerHTML = `
+        <img class="card-img" src="${item.image}" alt="Card image">
+        <h5 class="card-title">${item.title}</h5>
+        <h5 class="card-price">$${item.price}</h5>
+        <div class="item-counter">${item.count}</div>
+        <button onclick="handleDel(${item.id})" class="delBtns">Delete</button>
+      `;
+      cart.appendChild(div);
+    });
+  }
+  handle();
+}
+
+// Add the updateCart() call in the handleAdd() function
+
+function handleAdd(index) {
+  var obj = {
+    id: mydata[index].id,
+    image: mydata[index].image,
+    title: mydata[index].title,
+    price: mydata[index].price,
+  };
+  var x = JSON.parse(localStorage.getItem("data"));
+  if (x) {
+    var existingItem = x.find((item) => item.id === obj.id);
+    if (existingItem) {
+      existingItem.count++;
+    } else {
+      obj.count = 1;
+      x.push(obj);
+    }
+    localStorage.setItem("data", JSON.stringify(x));
+  } else {
+    obj.count = 1;
+    localStorage.setItem("data", JSON.stringify([obj]));
+  }
+  updateCart();
 }
