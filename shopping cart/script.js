6ac34fbdf9f8ce63736a07620361050963199fd9
Rohan Asif <rohanasif1990@gmail.com>
2023-06-01 10:35:53 +0500

    items correctly shown in cart div

--- shopping cart/script.js
+++ shopping cart/script.js
@@ -6,6 +6,7 @@ fetch("https://fakestoreapi.com/products")
   .then((response) => response.json())
   .then((data) => showData(data))
   .catch((error) => console.log(error));
+
 var mydata;
 function showData(d) {
   handle();
@@ -14,13 +15,30 @@ function showData(d) {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-            <img class="card-img" src="${item.image}" alt="Card image">
-            <h5 class="card-title">${item.title}</h5>
-            <h5 class="card-price">$${item.price}</h5>
-            <button onclick= "handleAdd(${index})" class="addBtns">Add to cart</button>
-        `;
+      <img class="card-img" src="${item.image}" alt="Card image">
+      <h5 class="card-title">${item.title}</h5>
+      <h5 class="card-price">$${item.price}</h5>
+      <button onclick= "handleAdd(${index})" class="addBtns">Add to cart</button>
+    `;
     container.appendChild(card);
   });
+
+  // Show items from localStorage in the cart at the first rendering
+  var storedData = JSON.parse(localStorage.getItem("data"));
+  if (storedData) {
+    storedData.forEach((item) => {
+      var div = document.createElement("div");
+      div.className = "item";
+      div.id = "item-" + item.id;
+      div.innerHTML = `
+        <img class="card-img" src="${item.image}" alt="Card image">
+        <h5 class="card-title">${item.title}</h5>
+        <h5 class="card-price">$${item.price}</h5>
+        <button onclick= "handleDel(${item.id})" class="delBtns">Delete</button>
+      `;
+      cart.appendChild(div);
+    });
+  }
 }
 
 function handleAdd(index) {
@@ -39,12 +57,12 @@ function handleAdd(index) {
   }
   var div = document.createElement("div");
   div.className = "item";
-  div.id = "item-" + obj.id; // Set the div id as item-{productId}
+  div.id = "item-" + obj.id;
   div.innerHTML = `
-              <img class="card-img" src="${obj.image}" alt="Card image">
-              <h5 class="card-title">${obj.title}</h5>
-              <h5 class="card-price">$${obj.price}</h5>
-              <button onclick= "handleDel(${obj.id})" class="delBtns">Delete</button>
+    <img class="card-img" src="${obj.image}" alt="Card image">
+    <h5 class="card-title">${obj.title}</h5>
+    <h5 class="card-price">$${obj.price}</h5>
+    <button onclick= "handleDel(${obj.id})" class="delBtns">Delete</button>
   `;
   cart.appendChild(div);
   handle();
@@ -57,19 +75,20 @@ cartIcon.addEventListener("click", () => {
 
 function handleDel(id) {
   var x = JSON.parse(localStorage.getItem("data"));
-  var index = x.findIndex((item) => item.id === id); // find the item to delete by id
+  var index = x.findIndex((item) => item.id === id);
   if (index > -1) {
     x.splice(index, 1);
     localStorage.setItem("data", JSON.stringify(x));
-    var itemElement = document.getElementById("item-" + id); // get the div element by id
+    var itemElement = document.getElementById("item-" + id);
     itemElement.remove();
     handle();
   }
 }
+
 let counter = document.getElementById("counter");
 function handle() {
   var a = JSON.parse(localStorage.getItem("data"));
-  if (a.length > 0) {
+  if (a && a.length > 0) {
     counter.innerHTML = a.length;
   } else {
     counter.innerHTML = "";
