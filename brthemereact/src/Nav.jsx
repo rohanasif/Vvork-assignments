0fb95d14eaee6bbd392713a4e90a513f83b6eef0
rohanasif1990 <rohanasif1990@gmail.com>
2023-06-15 13:55:48 +0500

    initial commit

---
+++ brthemereact/src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav class="py-2 px-4">
+      <a href="#home">
+        <h1 class="py-2 px-4 tracking-[4px] text-[15px]">
+          <b>BR</b> Architects
+        </h1>
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
