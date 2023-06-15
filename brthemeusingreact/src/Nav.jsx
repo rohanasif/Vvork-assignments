cc19a3224eaee5b94e2e9efb48cf285c1d900a43
Rohan Asif <rohanasif1990@gmail.com>
2023-06-15 11:27:57 +0500

    initial commit

---
+++ brthemeusingreact/src/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="">
+      <h1 class>
+        <strong>BR</strong> Architects
+      </h1>
+    </nav>
+  );
+};
+
+export default Nav;
