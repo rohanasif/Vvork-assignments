96261afc0c3c5b43d2bd69fff34f0da423f6712a
rohanasif1990 <rohanasif1990@gmail.com>
2023-06-15 14:04:09 +0500

    navbar complete

--- brthemereact/src/Nav.jsx
+++ brthemereact/src/Nav.jsx
@@ -2,12 +2,23 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav class="py-2 px-4">
+    <nav class="py-2 px-4 flex items-center justify-between">
       <a href="#home">
-        <h1 class="py-2 px-4 tracking-[4px] text-[15px]">
+        <h1 class="py-2 px-4 tracking-[4px] text-[15px] hover:bg-[#ccc]">
           <b>BR</b> Architects
         </h1>
       </a>
+      <ul class="flex">
+        <li class="py-2 px-4 tracking-[4px] text-[15px] hover:bg-[#ccc]">
+          Projects
+        </li>
+        <li class="py-2 px-4 tracking-[4px] text-[15px] hover:bg-[#ccc]">
+          About
+        </li>
+        <li class="py-2 px-4 tracking-[4px] text-[15px] hover:bg-[#ccc]">
+          Contact
+        </li>
+      </ul>
     </nav>
   );
 };
