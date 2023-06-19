91b0e9060f7cfabccef704cfa00596aca066925a
rohanasif1990 <rohanasif1990@gmail.com>
2023-06-19 12:18:19 +0500

    theme done

--- brthemereact/src/App.jsx
+++ brthemereact/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Nav from "./Nav";
 import Header from "./Header";
 import Main from "./Main";
-
+import Footer from "./Footer";
 const App = () => {
   return (
     <>
       <Nav />
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
