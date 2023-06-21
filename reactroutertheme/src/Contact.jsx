a35cefe713cbe5230b0373efc5f6d7897e4a44b5
rohanasif1990 <rohanasif1990@gmail.com>
2023-06-21 12:20:56 +0500

    all done

--- reactroutertheme/src/Contact.jsx
+++ reactroutertheme/src/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <>
       <div className="main">
-        <h1 className="main-text">Contact Form</h1>
+        <h1 className="main-text">CONTACT FORM</h1>
         <div className="second form">
           <input type="text" placeholder="Enter FullName" required />
           <input type="email" placeholder="Enter Email" required />
